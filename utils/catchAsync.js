1bda4af6736f17043e4f3572d58821da03913dec
Bilal Bin Shafiq <bilalbinshafiq195@gmail.com>
2022-08-14 23:17:23 +0500

    fresh commit

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,6 +1,20 @@
 /* eslint-disable arrow-body-style */
-module.exports = (fn) => {
-  return (request, response, next) => {
-    fn(request, response, next).catch(next);
-  };
-};
+module.exports = fn => {
+  return async (request, response, next) => {
+    try{
+      await fn(request, response, next);
+    } 
+    catch(error){
+      return response.status(500).json({
+        status: 'fails',
+        error,
+      })
+    }
+  }
+}
+
+// module.exports = (fn) => {
+//   return (request, response, next) => {
+//     fn(request, response, next).catch(next);
+//   };
+// };
